73f7baff07b1ec8078b1ca05c4429a58eae9dbba
Gianluca Della Vedova <gianluca@dellavedova.org>
2016-10-13 09:53:22 +0200

    Make script more robust

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,12 +2,12 @@
 set -e
 
 cd ~/pinw-deploy/Docker/
-v=`date +%FH%H%M`
+v=$(date +%FH%H%M)
 
-docker build -t algolab/pinw:${v} .
-for cont in `docker ps -f name=pinw -q`
+docker build -t "algolab/pinw:${v}" .
+for cont in $(docker ps -f name=pinw -q)
 do
-    docker rm -f $cont
+    docker rm -f "$cont"
 done
 
 echo -n "Checking if pinwnet network exists..."
@@ -18,7 +18,7 @@ echo "Halting and removing old pinw container"
 docker ps -a | grep pinw && docker stop pinw && docker rm -f pinw
 
 echo "Starting container pinw.${v} as $PINW_NAME"
-docker run -d  -v ~/pinw-data:/home/app/data --name "pinw" --net pinwnet --ip 172.18.10.10  -e APP_UID=$(id -u) -e APP_GID=$(id -g)  algolab/pinw:${v}
+docker run -d  -v ~/pinw-data:/home/app/data --name "pinw" --net pinwnet --ip 172.18.10.10  -e APP_UID="$(id -u)" -e APP_GID="$(id -g)"  "algolab/pinw:${v}"
 echo "Fix UID, GID"
 docker exec pinw /usr/local/sbin/inituidgid.sh
 echo "done"
