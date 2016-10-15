7faf44faf4a3f5e65232abe5f106edae0999ebad
Gianluca Della Vedova <gianluca@dellavedova.org>
2016-10-15 09:07:00 +0200

    Restart nginx
    At the end of the configuration, restart nginx. So we can be sure that
    the new configuration is used.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -19,6 +19,9 @@ docker ps -a | grep pinw && docker stop pinw && docker rm -f pinw
 
 echo "Starting container pinw.${v} as $PINW_NAME"
 docker run -d  -v ~/pinw-data:/home/data --name "pinw" --net pinwnet --ip 172.18.10.10  -e APP_UID="$(id -u)" -e APP_GID="$(id -g)"  "algolab/pinw:${v}"
+
+# Terrible hack, until I fix the docker data volume stuff
 echo "Fix UID, GID"
 docker exec pinw /usr/local/sbin/inituidgid.sh
+sv restart nginx
 echo "done"
